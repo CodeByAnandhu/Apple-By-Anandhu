42c0a4ca192df29716119c8e8bbc81e146099063
Anandhu Aji Gopinath <anandhuajigopinath@gmail.com>
2024-07-05 14:19:48 +0530

    smooth scrolling implimented

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,36 @@ import Model from './components/Model'
 import Features from './components/Features'
 import HowitWorks from './components/HowitWorks'
 import Footer from './components/Footer'
+import Lenis from '@studio-freight/lenis';
+import { useRef, useEffect } from 'react';
 import * as Sentry from "@sentry/react";
-const App = ()=> {
+
   
+const App = ()=> {
+
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    lenisRef.current = lenis;
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <main className="bg-black">
         <Navbar/>
